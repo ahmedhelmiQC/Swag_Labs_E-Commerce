845f07323385d1d08f4b49f2a0470624635896c8
ahmedhelmiQC <ahmed.systemtester@gmail.com>
2024-10-13 12:38:16 +0300

    Take Screen Shoot + Scrolling Using Actions

--- src/main/java/Utilities/LogsUtils.java
+++ src/main/java/Utilities/LogsUtils.java
@@ -1,4 +1,4 @@
 package Utilities;
 
-public class LogsUtil {
+public class LogsUtils {
 }
--- src/main/java/Utilities/Utility.java
+++ src/main/java/Utilities/Utility.java
@@ -1,19 +1,26 @@
 package Utilities;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import javax.swing.*;
+import java.io.File;
+import java.io.IOException;
 import java.time.Duration;
 
-public class Utility {
-                    /// Clicking //
+public class  Utility {
+
+
+                  /// Clicking //
     public static void clickingOnElement(WebDriver driver , By locator){
         new WebDriverWait(driver, Duration.ofSeconds(5))
                 .until(ExpectedConditions.elementToBeClickable(locator));
         driver.findElement(locator).click();
     }
-                /// Send Data//
+                       /// Send Data//
     public static void sendData(WebDriver driver,By locator, String data){
         new WebDriverWait(driver,Duration.ofSeconds(5))
                 .until(ExpectedConditions.visibilityOfElementLocated(locator));
@@ -25,7 +32,7 @@ public class Utility {
                 .until(ExpectedConditions.visibilityOfElementLocated(locator));
         driver.findElement(locator).getText();
     }
-                /// General Wait//
+                      /// General Wait//
     public static WebDriverWait generalWait(WebDriver driver){
         return new WebDriverWait(driver,Duration.ofSeconds(10));
     }
@@ -34,15 +41,33 @@ public class Utility {
     {
         return driver.findElement(locator);
     }
-            /// Scrolling ///
-    public static void scrolling(WebDriver driver,By locator){
-        ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView();",findWebElement(driver,locator))
+
+                         /// Scrolling Using Actions///
+    public static void scrollUsingActions (WebDriver driver , By locator){
+        Actions actions = new Actions(driver);
+        actions.scrollToElement(driver.findElement(locator)).perform();
+
     }
-            /// Drop Down ///
+                    /// Scrolling Using Javascript///
+    public static void scrolling(WebDriver driver,By locator){
+        ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView();",findWebElement(driver,locator));
+    }
+                      /// DropDown ///
     public static void dropDown(WebDriver driver , By locator){
         new WebDriverWait(driver,Duration.ofSeconds(5))
                 .until(ExpectedConditions.visibilityOfElementLocated(locator));
     }
+                      /// Take Screen Shoot //
+    public static void takeScreenShoot(WebDriver driver , String imageName) throws IOException {
+        String path = "test-outputs/screenshoot/";
+    File src =( (TakesScreenshot)driver).getScreenshotAs(OutputType.FILE); // Take a screenshot //
+    File target = new File(path+imageName+".png");  // Save the screenshot using Apache Commons IO
+
+        FileUtils.copyFile(src,target); /// copy the screenshot "src" file to the target location
+
+    }
+
+
 
 
 
